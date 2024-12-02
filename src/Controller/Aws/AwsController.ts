6804ebcc4e18a81a3c721e6f5b8f1ff9b055ea88
abdozaky45 <aa1066649@gmail.com>
2024-12-02 16:39:53 +0200

    some changes aws

--- src/Controller/Aws/AwsController.ts
+++ src/Controller/Aws/AwsController.ts
@@ -14,14 +14,14 @@ export const getPresignedURL = asyncHandler(
     }
     const aws_s3_service = new s3_service();
     const preSignedURLs = await Promise.all(
-      files.map(async (file: any, index: number) => {
+      files.map(async (file: {contentType:string,fileName?:string}, index: number) => {
         const fileName = `${req.body.fileName}/${
           req.body.currentUser.userInfo._id
         }_${Date.now()}_${index}`;
         const preSignedURL = await aws_s3_service.createPresignedUrlWithClient({
           region: regionAws,
           bucket: bucketName,
-          key: fileName,
+          key: file.fileName || fileName,
           contentType: file.contentType || "image/jpeg",
         });
         return {
