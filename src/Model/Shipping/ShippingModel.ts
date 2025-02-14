e7dad6afdd5552948bdba89e9467825dc37b6b7f
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2025-02-14 17:59:09 +0200

    Refactor shipping model and service to include isDeleted property for soft deletion

--- src/Model/Shipping/ShippingModel.ts
+++ src/Model/Shipping/ShippingModel.ts
@@ -1,10 +1,11 @@
 import { Schema, model } from "mongoose"
-import { RequiredNumber, RequiredUniqueString } from "../../Utils/Schemas";
+import { NotRequiredBoolean, RequiredNumber, RequiredUniqueString } from "../../Utils/Schemas";
 import SchemaTypesReference from "../../Utils/Schemas/SchemaTypesReference";
 import IShipping from "./Ishipping";
 const ShippingSchema = new Schema<IShipping>({
     category: RequiredUniqueString,
-    cost: RequiredNumber
+    cost: RequiredNumber,
+    isDeleted:NotRequiredBoolean
 });
 const ShippingModel = model(SchemaTypesReference.Shipping, ShippingSchema)
 export default ShippingModel;
