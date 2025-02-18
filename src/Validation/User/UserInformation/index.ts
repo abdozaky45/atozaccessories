2a635e0c653f0c7495c345146dbb5f421313d749
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2025-02-18 19:41:22 +0200

    Update user validation schema to refine apartmentSuite and phone number patterns

--- src/Validation/User/UserInformation/index.ts
+++ src/Validation/User/UserInformation/index.ts
@@ -7,11 +7,11 @@ export const createUser = baseSchema.concat(
       firstName: joi.string().min(3).max(50).required(),
       lastName: joi.string().min(3).max(50).required(),
       address: joi.string().min(3).max(200).required(),
-      apartmentSuite: joi.string().min(3).max(60).allow("").optional(),
+      apartmentSuite: joi.string().min(1).max(200).allow("").optional(),
       shipping: joi.string().required(),
       postalCode: joi.string().min(3).max(6).allow("").optional(),
-      primaryPhone: joi.string().pattern(/^(\+?2)?01\d{9}$/).required(),
-      secondaryPhone: joi.string().pattern(/^(\+?2)?01\d{9}$/).allow("").optional(),
+      primaryPhone: joi.string().pattern(/^(\+?2)?01[0-25]\d{8}$/).required(),
+      secondaryPhone: joi.string().pattern(/^(\+?2)?01[0-25]\d{8}$/).allow("").optional(),      
     })
     .required()
 );
@@ -22,17 +22,12 @@ export const updateUser = baseSchema.concat(
       firstName: joi.string().min(3).max(50).optional(),
       lastName: joi.string().min(3).max(50).optional(),
       address: joi.string().min(3).max(200).optional(),
-      apartmentSuite: joi.string().min(3).max(60).optional().allow(""),
+      apartmentSuite: joi.string().min(1).max(200).optional().allow(""),
       shipping: joi.string().optional(),
       postalCode: joi.string().min(3).max(6).optional().allow(""),
-      primaryPhone: joi
-        .string()
-        .pattern(/^(\+?2)?01\d{9}$/)
-        .optional(),
-      secondaryPhone: joi
-        .string()
-        .pattern(/^(\+?2)?01\d{9}$/).allow("")
-        .optional(),
+      primaryPhone: joi.string().pattern(/^(\+?2)?01[0-25]\d{8}$/).optional(),
+      secondaryPhone: joi.string().pattern(/^(\+?2)?01[0-25]\d{8}$/).allow("").optional(),
+      
     })
     .required()
 );
