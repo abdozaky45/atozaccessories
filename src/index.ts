6f05fa0b64e02f8bc60e8e64a0b101a98927c764
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2025-02-20 22:45:50 +0200

    Remove custom CORS options and simplify server startup code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
 import { app } from "./app";
 import DbConnection from "./DbSetup/DbConfig";
-import { findUserInformationById } from "./Service/User/AuthService";
 DbConnection();
-app.listen(process.env.PORT,async() => {
- 
+app.listen(process.env.PORT, async () => {
   console.log(`server is running on port ${process.env.PORT}`);
 });
